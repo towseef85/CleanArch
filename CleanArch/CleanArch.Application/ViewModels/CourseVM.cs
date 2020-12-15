908c36fcd4e822fcaae918bfc1e23c03d4354502
Towseef Ahmed <towseef_ahmed85@outlook.com>
2020-12-15 23:59:51 +0530

    Adding Interfaces for courses

---
+++ CleanArch/CleanArch.Application/ViewModels/CourseVM.cs
@@ -0,0 +1,12 @@
+﻿using CleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.ViewModels
+{
+    public class CourseVM
+    {
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
