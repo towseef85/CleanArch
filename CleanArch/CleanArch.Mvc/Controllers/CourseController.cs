11a638af55d41bb38c1aa2f802f3c19607386c60
Towseef Ahmed <towseef_ahmed85@outlook.com>
2020-12-17 07:56:41 +0530

    Commit changes for course presentation

---
+++ CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseService _icourseService;
+
+        public CourseController(ICourseService icourseService)
+        {
+            _icourseService = icourseService;
+        }
+        public IActionResult Index()
+        {
+            CourseVM model = _icourseService.GetCourses();
+            return View(model);
+        }
+    }
+}
