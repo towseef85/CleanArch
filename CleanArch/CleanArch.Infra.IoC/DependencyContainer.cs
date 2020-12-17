9c0e33a087b95cc2b041499628cb92f7fa9c63b7
Towseef Ahmed <towseef_ahmed85@outlook.com>
2020-12-17 12:37:35 +0530

    Added Commands and updated course repo and cleaned up the IOC container

--- CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
+++ CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
@@ -1,6 +1,9 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.Services;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Bus;
+using CleanArch.Infra.Data.Context;
 using CleanArch.Infra.Data.Repository;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -13,8 +16,10 @@ namespace CleanArch.Infra.IoC
     {
         public static void RegisterService(IServiceCollection service)
         {
+            service.AddScoped<IMediatRHandler, InMemoryBus>();
             service.AddScoped<ICourseService, CourseService>();
             service.AddScoped<ICourseRepository, CourseRepository>();
+            service.AddScoped<UniversityDbContext>();
         }
     }
 }
